f4dffac63da9003b8c06a40320720a2fc9105367
Drelf2018 <drelf2018@outlook.com>
2022-12-01 02:38:12 +0800

    feat: 新增提示

--- src/components/Show.vue
+++ src/components/Show.vue
@@ -26,7 +26,8 @@ export default {
         var roomid = this.$route.params.roomid
         if(roomid == "redirect") {
             roomid = this.$route.query.roomid || 21452505
-            this.dms.push(makeDanmaku(`请使用新网址 https://danmu.nana7mi.link/${roomid}`))
+            this.dms.push(makeDanmaku("弹幕姬网址已更换"))
+            this.dms.push(makeDanmaku(`danmu.nana7mi.link/${roomid}`))
         }
         await onClickShow(roomid, this.dms, this.$route.query.price || 9.9)
         document.getElementById("app").style.zoom = this.$route.query.zoom || 1
